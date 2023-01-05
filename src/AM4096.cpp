d14dbd2659a7b5232bee046a5c7200f4b942e829
Yasir Shahzad <Yasirshahzad918@gmail.com>
2023-01-05 05:39:17 +0500

    Update

---
+++ src/AM4096.cpp
@@ -0,0 +1,283 @@
+/** @file AM4096.cpp
+ * AM4096 interface library for mbed framework
+ * Copyright (C)  Yasir Shahzad
+ *
+ * Distributed under the MIT license.
+ * For full terms see the file LICENSE.md.
+ */
+#include "AM4096.h"
+
+#if AM4096_LOGS
+    #define AM_LOG(f_ , ...) printf((f_), ##__VA_ARGS__)
+#else
+    #define AM_LOG(f_ , ...) do {} while (0)
+#endif
+
+#if AM4096_LOGS
+static const char CONFIG_STR[] = "*******CONFIG*******\r\n"
+                                 "Addr    : 0x%03X\r\n"
+                                 "Reg35   : 0x%03X\r\n"
+                                 "Pdie    : 0x%03X\r\n"
+                                 "Pdtr    : 0x%03X\r\n"
+                                 "Slowint : 0x%03X\r\n"
+                                 "AGCdis  : 0x%03X\r\n"
+                                 "Pdint   : 0x%03X\r\n"
+                                 "Zin     : 0x%03X\r\n"
+                                 "Sign    : 0x%03X\r\n"
+                                 "Bufsel  : 0x%03X\r\n"
+                                 "Abridis : 0x%03X\r\n"
+                                 "Hist    : 0x%03X\r\n"
+                                 "Daa     : 0x%03X\r\n"
+                                 "Nfil    : 0x%03X\r\n"
+                                 "Res     : 0x%03X\r\n"
+                                 "UVW     : 0x%03X\r\n"
+                                 "Sth     : 0x%03X\r\n"
+                                 "SSIcfg  : 0x%03X\r\n"
+                                 "Dac     : 0x%03X\r\n"
+                                 "Dact    : 0x%03X\r\n"
+                                 "*******************\r\n";
+
+static const char OUTPUT_STR[] = "*******OUTPUT*******\r\n"
+                                 "Rpos    : 0x%03X\r\n"
+                                 "SRCH    : 0x%03X\r\n"
+                                 "Apos    : 0x%03X\r\n"
+                                 "SRCH    : 0x%03X\r\n"
+                                 "Wel     : 0x%03X\r\n"
+                                 "Weh     : 0x%03X\r\n"
+                                 "Tho     : 0x%03X\r\n"
+                                 "Thof    : 0x%03X\r\n"
+                                 "AGCgain : 0x%03X\r\n"
+                                 "********************\r\n";
+#else
+static const char CONFIG_STR[] = "";
+static const char OUTPUT_STR[] = "";
+#endif
+
+AM4096::AM4096(I2C *i2c_instance, uint8_t hw_addr)
+    : _i2c(i2c_instance), _hw_addr(hw_addr), _device_id(0), _initialised(false)
+{
+    for(int i=0; i<AM4096_CONFIG_DATA_LEN; i++)
+        _configuration.data[i]=0;
+}
+
+int AM4096::init()
+{
+    if (_initialised)
+        return 0;
+    wait_ms(30); // POWER-UP
+    int attempts = 0;
+    AM_LOG("Connection attempt ...\r\n");
+    while (readReg(AM4096_REGISTER_CONFIG_DATA_ADDR, &_configuration.data[0]))
+    {
+        attempts += 1;
+        AM_LOG("Attempt %d failed...\r\n",attempts);
+        if(attempts == 3)
+        {
+            AM_LOG("There is no device with this address!\r\n");
+            return 1;
+        }
+    }
+    AM_LOG("Connection succesfull ...\r\n");
+    AM_LOG("Device addr: 0x%02X\r\n", _configuration.fields.Addr);
+    
+    
+    uint16_t id_buffer[2];
+    for(int i=0;i<AM4096_EEPROM_DEVICE_ID_LEN;i++)
+        readReg(AM4096_EEPROM_DEVICE_ID_ADDR+i, id_buffer+i);
+    _device_id = (uint32_t)(id_buffer[0] << 16) | id_buffer[1];
+    
+    AM_LOG("Device id: 0x%08X\r\n");
+
+    for(int i=0; i<AM4096_CONFIG_DATA_LEN; i++)
+        readReg(AM4096_EEPROM_CONFIG_DATA_ADDR+i, &_configuration.data[i]);
+    
+    printAM4096Configuration(&_configuration);
+
+    _initialised = true;
+    return 0;
+}
+
+int AM4096::readReg(uint8_t addr, uint16_t * reg)
+{
+    char buffer[2];
+    int status = 0;
+    status += _i2c->write((int)_hw_addr<<1, (const char*)&addr, 1, true);
+    if(addr <= 0x1F)
+        wait_us(20); // EEPROM CLK stretching
+    status += _i2c->read((int)_hw_addr<<1, buffer,2,false);
+    *reg = (uint16_t)((buffer[0] << 8) & 0xff00) | (uint16_t)buffer[1];
+    return status; 
+}
+
+int AM4096::writeReg(uint8_t addr, uint16_t * reg)
+{
+    bool flag = !((addr < (AM4096_EEPROM_CONFIG_DATA_ADDR + AM4096_CONFIG_DATA_LEN)) ||
+        ((addr >=AM4096_REGISTER_CONFIG_DATA_ADDR) && 
+        (addr < (AM4096_REGISTER_CONFIG_DATA_ADDR + AM4096_CONFIG_DATA_LEN) )));
+    if(flag)
+        return 1;
+    
+    char buffer[3];
+    buffer[0] = addr; 
+    buffer[1] = (*reg >> 8) & 0xFF; 
+    buffer[2] = *reg & 0xFF;
+    int status = _i2c->write((int)_hw_addr<<1, (const char *)buffer, 3, false); // wait ~ 20ms after writing to EEPROM 
+    if(addr < (AM4096_EEPROM_CONFIG_DATA_ADDR + AM4096_CONFIG_DATA_LEN))
+        wait_ms(AM4096_EEPROM_WRITE_TIME+2);
+    return status;
+} 
+
+int AM4096::findAM4096Device()
+{
+    int found_flag = 1, last_hw_addr = _hw_addr;
+    AM_LOG("Starting searching procedure...\r\n");
+    _hw_addr = AM4096_ADDR_FIRST;
+
+    while(found_flag && (_hw_addr <= AM4096_ADDR_LAST))
+    {
+        if(found_flag = readReg(AM4096_REGISTER_CONFIG_DATA_ADDR, &_configuration.data[0]))
+        {
+            _hw_addr++;
+            wait_ms(10);
+        }
+    }
+    if(found_flag != AM4096_ERROR_NONE)
+    {
+        _hw_addr = last_hw_addr;
+        AM_LOG("No devices found!\r\n");
+        return 1;
+    }
+
+    AM_LOG("Device with addr: 0x%02X found!\r\n", _hw_addr);
+    return 0;
+}
+
+uint32_t AM4096::getDeviceId()
+{
+    return _device_id;
+}
+
+int AM4096::setNewHwAddr(uint8_t hw_addr)
+{
+    if(hw_addr > 0x7F && _initialised)
+    {
+        AM_LOG("Can't set new addres!\r\n");
+        return 1;
+    }
+    _configuration.fields.Addr = hw_addr;
+    int result = writeReg(AM4096_EEPROM_CONFIG_DATA_ADDR,&_configuration.data[0]);
+    if(result)
+    {
+        AM_LOG("Can't set new addres!\r\n");
+        _configuration.fields.Addr = _hw_addr;
+        return 1; 
+    }
+    _hw_addr = hw_addr;
+    AM_LOG("New addr 0x%02X set!\r\n", hw_addr);
+    return 0;
+}
+
+void AM4096::getConfiguration(AM4096_config_data * conf_ptr)
+{
+    MBED_ASSERT(conf_ptr);
+    *conf_ptr = _configuration;
+}
+
+void AM4096::printAM4096Configuration(const AM4096_config_data * conf_ptr)
+{
+    if(conf_ptr == 0)
+        return;
+    AM_LOG(
+    CONFIG_STR,
+    conf_ptr->fields.Addr,
+    conf_ptr->fields.Reg35,
+    conf_ptr->fields.Pdie,
+    conf_ptr->fields.Pdtr,
+    conf_ptr->fields.Slowint,
+    conf_ptr->fields.AGCdis,
+    conf_ptr->fields.Pdint,
+    conf_ptr->fields.Zin,
+    conf_ptr->fields.Sign,
+    conf_ptr->fields.Bufsel,
+    conf_ptr->fields.Abridis,
+    conf_ptr->fields.Hist,
+    conf_ptr->fields.Daa,
+    conf_ptr->fields.Nfil,
+    conf_ptr->fields.Res,
+    conf_ptr->fields.UVW,
+    conf_ptr->fields.Sth,
+    conf_ptr->fields.SSIcfg,
+    conf_ptr->fields.Dac,
+    conf_ptr->fields.Dact
+    );
+}
+
+void AM4096::printAM4096OutputData(const AM4096_output_data * out_ptr)
+{
+    if(out_ptr == 0)
+        return;
+    AM_LOG(
+        OUTPUT_STR,
+        out_ptr->fields.Rpos,
+        out_ptr->fields.SRCHRpos,
+        out_ptr->fields.Apos,
+        out_ptr->fields.SRCHApos,
+        out_ptr->fields.Wel,
+        out_ptr->fields.Weh,
+        out_ptr->fields.Tho,
+        out_ptr->fields.Thof,
+        out_ptr->fields.AGCgain
+    );
+}
+
+int AM4096::updateConfiguration(const AM4096_config_data * conf_ptr, bool permament)
+{
+    MBED_ASSERT(conf_ptr);
+
+    if(conf_ptr->fields.Addr != _configuration.fields.Addr)
+        return 1;
+    
+    int status = 0;
+    if(permament)
+    {
+        // check if already in memory
+        for(int i=0;i<4;i++)
+        {
+            if(_configuration.data[i] == conf_ptr->data[i])
+                status++;
+        }
+        if(status == 4)
+        {
+            AM_LOG("Configuration is identical to the one in the EEPROM!\r\n");
+            return 0;
+        }
+        status = 0;
+    }
+    uint16_t buffer[4];
+    for(int i=0;i<4;i++) buffer[i] = conf_ptr->data[i];
+    if(permament)
+    {
+        for (int i=0; i < AM4096_CONFIG_DATA_LEN; i++)
+            status += writeReg(AM4096_EEPROM_CONFIG_DATA_ADDR + i, buffer+i);
+    }
+    else
+    {
+        for (int i = 0; i < AM4096_CONFIG_DATA_LEN; i++)
+            status += writeReg(AM4096_REGISTER_CONFIG_DATA_ADDR + i, buffer+i);
+    }
+    if(status!=AM4096_ERROR_NONE)
+        return status;
+    
+    for(int i=0;i<4;i++) _configuration.data[i] = buffer[i];
+    AM_LOG("Configuration succesfully written to memory!\r\n");
+    return status;
+}
+
+int AM4096::readOutputDataRegisters(AM4096_output_data * out_ptr)
+{
+    MBED_ASSERT(out_ptr);
+    int status = 0;
+    for(int i = 0; i < AM4096_REGISTER_MEAS_DATA_LEN; i++)
+        status = readReg(AM4096_REGISTER_MEAS_DATA_ADDR + i, &out_ptr->data[i]);
+    return status;
+}
---
+++ src/AM4096.h
@@ -0,0 +1,226 @@
+/** @file AM4096.h
+ * AM4096 interface library for mbed framework
+ * Copyright (C)  Yasir Shahzad
+ *
+ * Distributed under the MIT license.
+ * For full terms see the file LICENSE.md.
+ */
+#ifndef __AM4096_H__
+#define __AM4096_H__
+
+#include <mbed.h>
+
+#define AM4096_MEMORY_WORD_LEN              2   ///< AM4096 memory word length in Bytes
+#define AM4096_EEPROM_CONFIG_DATA_ADDR      0   ///< address of first word of AM4096 configuration in EEPROM  
+#define AM4096_REGISTER_CONFIG_DATA_ADDR    48  ///< address of first word of AM4096 configuration in registers
+#define AM4096_CONFIG_DATA_LEN              4   ///< AM4096 configuration length in words
+#define AM4096_EEPROM_DEVICE_ID_ADDR        30  ///< addres of first word of AM4096 ID
+#define AM4096_EEPROM_DEVICE_ID_LEN         2   ///< AM4960 ID length in words
+#define AM4096_REGISTER_MEAS_DATA_ADDR      32  ///< address of first word of AM4096 measurement data in registers
+#define AM4096_REGISTER_MEAS_DATA_LEN       4   ///< AM4096 measurement data length in words
+#define AM4096_ADDR_FIRST                   0   ///< AM4096 default hw address and begining of device's address space (I2C) 
+#define AM4096_ADDR_LAST                    127 ///< end of device's address space (I2C)                 
+#define AM4096_ERROR_NONE                   0   ///< no error <==> success, non zero return code indicates error
+#define AM4096_EEPROM_WRITE_TIME            20  ///< EEPROM write time in ms
+
+/**
+ * @brief AM4096 configuration data structure.
+ *
+ * More specific information about particular settings and the correct values you can
+ * find in the product's datasheet: 
+ * https://www.rls.si/en/fileuploader/download/download/?d=0&file=custom%2Fupload%2FAM4096D02_06_EN_data_sheet.pdf
+ * or on product's webpage:
+ * https://www.rls.si/en/am4096-12-bit-rotary-magnetic-encoder-chip
+ * 
+ * @sa AM4096_meas_fields
+ */
+struct AM4096_config_fields
+{
+    uint16_t Addr    : 7, ///< Device I2C address
+                     : 1,
+             Reg35   : 1, ///< Regulator voltage
+             Pdie    : 1, ///< Internal power down
+             Pdtr    : 2, ///< Internal power down rate
+             Slowint : 1, ///< Interpolator delay
+                     : 1,
+             AGCdis  : 1, ///< AGC disable
+             Pdint   : 1; ///< Interpolator power
+    uint16_t Zin     : 12,///< Zero position data
+             Sign    : 1, ///< Select the output direction
+                     : 1,
+             Bufsel  : 1, ///< Selects the output on pins U/Nsin, V/Psin, W/Ncos, Td/Pcos
+             Abridis : 1; ///< Enabling A B Ri outputs
+    uint16_t Hist    : 7, ///< Digital hysteresis value in LSB at 12bit resolution
+             Daa     : 1, ///< Output position selection
+             Nfil    : 8; ///< Test parameters
+    uint16_t Res     : 3, ///< Interpolation factor rate
+             UVW     : 3, ///< UVW number of periods/turn
+             Sth     : 3, ///< Tacho measuring range
+                     : 2,
+             SSIcfg  : 2, ///< SSI settings
+             Dac     : 2, ///< Linear voltage period selection
+             Dact    : 1; ///< Select the output on Vout/Tout pin
+};
+
+/**
+ * @brief AM4096 output data structure.
+ *
+ * More specific information about particular settings and the correct values you can
+ * find in the product's datasheet: 
+ * https://www.rls.si/en/fileuploader/download/download/?d=0&file=custom%2Fupload%2FAM4096D02_06_EN_data_sheet.pdf
+ * or on product's webpage:
+ * https://www.rls.si/en/am4096-12-bit-rotary-magnetic-encoder-chip
+ * 
+ * @sa AM4096_config_fields
+ */
+struct AM4096_meas_fields
+{
+    uint16_t Rpos       : 12, ///< Relative position inf.
+                        : 3,  
+             SRCHRpos   : 1;  ///< Relative position output valid
+    uint16_t Apos       : 12, ///< Absolute position inf.
+                        : 3,
+             SRCHApos   : 1;  ///< Absolute position outpout valid
+    uint16_t            : 13,
+             Wel        : 1,  ///< Magnet too close status
+             Weh        : 1,  ///< Magnet too far status
+                        : 1;
+    uint16_t Tho        : 10, ///< Tacho output data
+             Thof       : 1,  ///< Tacho overflow info
+                        : 1,
+             AGCgain    : 4;  ///< AGC disable
+};
+
+
+/**
+ * @brief Configuration data helper union.
+ * @sa AM4096_config_fields 
+ */
+union AM4096_config_data
+{
+    AM4096_config_fields fields;
+    uint16_t data[4];
+};
+
+/**
+ * @brief Output data helper union.
+ * @sa AM4096_meas_fields 
+ */
+union AM4096_output_data
+{
+    AM4096_meas_fields fields;
+    uint16_t data[4];
+};
+
+/**
+ * @brief AM4096 interface class.
+ *
+ * It allows to program AM4096 encoder and read its output and configuration registers.
+ * @sa AM4096_config_data
+ * @sa AM4096_output_data
+ */
+class AM4096  
+{
+    public:
+        /**
+         * @brief Create AM4096 interface.
+         * @param i2c_instance pointer to I2C instance
+         * @param hw_addr device address 
+         * @return 
+         */
+        AM4096(I2C * i2c_instance, uint8_t hw_addr);
+
+        /**
+         * @brief Initialise device.
+         *
+         * It initialise communication with device and reads its configuration and ID. If address is incorrect it starts
+         * the search procedure to find first valid device.
+         * @sa findAM4096Device() 
+         * @return AM4096_ERROR_NONE if success, non zero value on failure
+         */
+        int init();
+        
+        /**
+         * @brief Get device ID.
+         * @return device id 
+         */
+        uint32_t getDeviceId();
+        
+        /**
+         * @brief Start search procedure on i2c bus.
+         *
+         * It starts search procedure to look for first valid device. If found, class is initialised with this address.
+         * You must call init() if device was found to use it. 
+         * @return AM4096_ERROR_NONE if success, non zero value on failure
+         */
+        int findAM4096Device();
+        
+        /**
+         * @brief Set new device address.
+         * @param hw_addr new address   
+         * @return AM4096_ERROR_NONE if success, non zero value on failure
+         */
+        int setNewHwAddr(uint8_t hw_addr);
+        
+        /**
+         * @brief Get current configuration.
+         * @param conf_ptr pointer to configuration object to update 
+         */
+        void getConfiguration(AM4096_config_data * conf_ptr);
+        
+        /**
+         * @brief Write new configuration to device.
+         * @param conf_ptr pointer to configuration object 
+         * @param permament if true device's EEPROM is update, otherwise only registers are udpate and configuration will be lost after power cycle
+         * @return AM4096_ERROR_NONE if success, non zero value on failures  
+         */
+        int updateConfiguration(const AM4096_config_data * conf_ptr, bool permament=false);
+        
+        /**
+         * @brief Read measurement data from device registers.
+         * @param out_ptr pointer to output object to update
+         * @return AM4096_ERROR_NONE if success, non zero value on failures   
+         */
+        int readOutputDataRegisters(AM4096_output_data * out_ptr);
+        
+        /**
+         * @brief Prints formatted table with configuration registers.
+         *
+         * Works only if AM4096_LOGS is defined and set to 1
+         * @param conf_ptr pointer to configuration to print out
+         */
+        static void printAM4096Configuration(const AM4096_config_data * conf_ptr);
+
+        /**
+         * @brief Prints formatted table with measurement registers.
+         *
+         * Works only if AM4096_LOGS is defined and set to 1
+         * @param out_ptr pointer to configuration to print out
+         */
+        static void printAM4096OutputData(const AM4096_output_data * out_ptr);
+
+        /**
+         * @brief Write a 16bit word into AM4096 register/EEPROM.
+         * @param addr register address
+         * @param reg pointer to word
+         * @return AM4096_ERROR_NONE if success, non zero value on failures 
+         */
+        int readReg(uint8_t addr, uint16_t * reg);
+
+        /**
+         * @brief Read a 16bit word from AM4096 register/EEPROM.
+         * @param addr register address 
+         * @param reg pointer to word
+         * @return AM4096_ERROR_NONE if success, non zero value on failures 
+         */
+        int writeReg(uint8_t addr, uint16_t * reg); 
+    
+    private:
+        I2C * _i2c;
+        uint8_t _hw_addr;
+        uint32_t _device_id;
+        bool _initialised;
+        AM4096_config_data _configuration;
+};
+
+#endif /* __AM4096_H__ */
