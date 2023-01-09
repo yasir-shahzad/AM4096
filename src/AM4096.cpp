50995dfb3882a31e63b3eec9e726d5ffb53e738b
Yasir Shahzad <Yasirshahzad918@gmail.com>
2023-01-10 01:32:03 +0500

    Update.

--- src/AM4096.cpp
+++ src/AM4096.cpp
@@ -14,19 +14,6 @@
 #endif
 
 
-void pr(char *fmt, ... )
-{
- char buf[128]; // resulting string limited to 128 chars
-
- va_list args;
- va_start (args, fmt );
- vsnprintf(buf, 128, fmt, args);
- va_end (args);
-
- Serial.println(buf);
-}
-
-
 #if AM4096_LOGS
 static const char CONFIG_STR[] = "*******CONFIG*******\r\n"
                                  "Addr    : 0x%03X\r\n"
